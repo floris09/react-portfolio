319c93328d41f2f5fe5f2fe18fffb12a65ee1549
floris09 <florismeininger@gmail.com>
2018-02-05 17:31:49 +0100

    add new image

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -6,11 +6,11 @@ class Home extends PureComponent {
   render() {
     return (
       <div>
-        <div className='background background1'><i class="fas fa-angle-double-down"></i></div>
+        <div className='background1'><i class="fas fa-angle-double-down"></i></div>
 
         <div className='black-bar'></div>
 
-        <div className='background background2'>
+        <div className='background2'>
           <div className='whitebox'>
             <About />
           </div>
@@ -18,15 +18,15 @@ class Home extends PureComponent {
 
         <div className='black-bar'></div>
 
-        <div className='background background3'></div>
+        <div className='background3'></div>
 
         <div className='black-bar'></div>
 
-        <div className='background background4'></div>
+        <div className='background4'></div>
 
         <div className='black-bar'></div>
 
-        <div className='background background5'></div>
+        <div className='background5'></div>
 
 
       </div>
