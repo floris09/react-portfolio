f063bf7673193f0a47d88ed77b73bce236737e41
floris09 <florismeininger@gmail.com>
2018-02-06 13:38:41 +0100

    change api address

--- src/api/client.js
+++ src/api/client.js
@@ -4,7 +4,7 @@ export default class ApiClient {
 
   constructor(host, options = {}) {
     this.host = process.env.NODE_ENV === 'production'
-      ? 'https://expert-chinbone.codaisseur.cloud' // WITHOUT the / !!!
+      ? 'https://florismeininger-mailer-api.herokuapp.com'
       : (host || 'http://localhost:3030')
 
     this.options = { ...options }
